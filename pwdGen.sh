ef25e274a9691b8537ddc944e7d2d59f863e90f1
zykxs <af48348@gmail.com>
2023-12-13 10:32:01 +0000

    Update: Pede ao usar o número de passwords.

--- pwdGen.sh
+++ pwdGen.sh
@@ -5,17 +5,17 @@ echo
 
 # Função p gerar password
 generate_password() {
-    local password=$(tr -dc 'A-Za-z0-9!@#$%^&*()' < /dev/random | head -c "$n_chars")
+    local password=$(tr -dc 'A-Za-z0-9!@#$%^&*()' < /dev/random | head -c "$chars")
     echo "$password"
 }
 
 # Main script
-read -p "nº de caractéres: " n_chars
-read -p "nº de passwords: " n_passwords
+read -p "nº de caractéres: " nChars
+read -p "nº de passwords: " nPasswords
 echo
 
     # Ciclo pa gerar n passwords
-for ((i = 0; i < $n_passwords; i++)); do
-    password=$(generate_password "$n_chars")
+for ((i = 0; i < $nPasswords; i++)); do
+    password=$(generate_password "$nChars")
     echo "Pwd $((i+1)): $password"
 done
