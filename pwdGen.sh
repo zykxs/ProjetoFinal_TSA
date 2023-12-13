6c95603655da4f5e00f312f779ae7a0fc6557a61
zykxs <af48348@gmail.com>
2023-12-13 10:21:17 +0000

    Update: Pede ao usar quantas passwords quer.

--- pwdGen.sh
+++ pwdGen.sh
@@ -3,16 +3,19 @@
 #!/bin/bash
 echo
 
-#Função p gerar password
+# Função p gerar password
 generate_password() {
-    local password=$(tr -dc 'A-Za-z0-9!@#$%^&*()' < /dev/urandom | head -c "$password_length")
+    local password=$(tr -dc 'A-Za-z0-9!@#$%^&*()' < /dev/random | head -c "$n_chars")
     echo "$password"
 }
 
 # Main script
-read -p "Introduza nº de caractéres: " password_length
-
-password=$(generate_password "$password_length")
-
+read -p "nº de caractéres: " n_chars
+read -p "nº de passwords: " n_passwords
 echo
-echo "Password: $password/n"
+
+    # Ciclo pa gerar n passwords
+for ((i = 0; i < $n_passwords; i++)); do
+    password=$(generate_password "$n_chars")
+    echo "Pwd $((i+1)): $password"
+done
